22622dd153c62a9ffe0b3110e061b5241b5e42eb
3pic_Ozone <3pic.ozone@gmail.com>
2018-10-07 12:21:05 -0500

    refactor

--- place/models.py
+++ place/models.py
@@ -8,15 +8,16 @@ class Info(models.Model):
     def __str__(self):
         return self.name
 
+class Picture(models.Model):
+    picture = models.FileField(upload_to='uploads/')
+    def __str__(self):
+        return "Point: " + str(self.id)
+
 class Point(models.Model):
     info = models.ForeignKey(Info, on_delete=models.CASCADE)
+    pic = models.ForeignKey(Picture, on_delete=models.CASCADE)
     latitude = models.FloatField(max_length=25)
     longitude = models.FloatField(max_length=25)
     def __str__(self):
         return "Point: " + str(self.info.name)
 
-class Picture(models.Model):
-    info = models.ForeignKey(Info, on_delete=models.CASCADE)
-    picture = models.FileField(upload_to='uploads/')
-    def __str__(self):
-        return "Point: " + str(self.info.name)
